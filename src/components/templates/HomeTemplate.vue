55f418ab50f2ff313f29346e253d71ca474d7683
Felipe César <jeanfelipecc@gmail.com>
2018-10-02 09:47:42 -0300

    add components into HomeTemplate

--- src/components/templates/HomeTemplate.vue
+++ src/components/templates/HomeTemplate.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="home"/>
+  <div class="home">
+    <page-header/>
+    <movie-list/>
+  </div>
 </template>
 
 <script>
+import PageHeader from '@/components/organisms/PageHeader.vue';
+import MovieList from '@/components/organisms/MovieList.vue';
 
 export default {
+  components: {
+    PageHeader,
+    MovieList
+  }
 }
 </script>
 
@@ -16,4 +25,3 @@ export default {
   padding: 0 36px;
 }
 </style>
-

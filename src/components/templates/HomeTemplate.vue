f4f370354aaeb05803cd297488cf749e8b3b5730
Felipe César <jeanfelipecc@gmail.com>
2018-10-02 09:24:22 -0300

    change components structure

---
+++ src/components/templates/HomeTemplate.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home"/>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+.home {
+  display: grid;
+  grid-column: 1 / -1;
+  grid-gap: 30px;
+  padding: 0 36px;
+}
+</style>
+

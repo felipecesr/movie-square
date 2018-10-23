a9e243b12dde851a74ebf3103185c4eeda996d8f
Felipe César <jeanfelipecc@gmail.com>
2018-10-23 13:35:18 -0300

    Add MovieList infinite scroll

--- src/components/organisms/MovieList.vue
+++ src/components/organisms/MovieList.vue
@@ -14,13 +14,43 @@ import MovieItem from "@/components/molecules/MovieItem.vue";
 export default {
   components: { MovieItem },
 
-  computed: mapState(['movies']),
-
-  beforeMount() {
-    this.fetchMovies({ type: 'movie' });
+  data() {
+    return {
+      bottom: false
+    };
   },
 
-  methods: mapActions(['fetchMovies'])
+  computed: mapState(['movies']),
+
+  watch: {
+    bottom(bottom) {
+      if (bottom) {
+        this.fetchMovies();
+      }
+    }
+  },
+
+  created() {
+    window.addEventListener('scroll', () => {
+      this.bottom = this.bottomVisible();
+    })
+  },
+
+  beforeMount() {
+    this.fetchMovies();
+  },
+
+  methods: {
+    ...mapActions(['fetchMovies']),
+
+    bottomVisible() {
+      const scrollY = window.scrollY;
+      const visible = document.documentElement.clientHeight;
+      const pageHeight = document.documentElement.scrollHeight;
+      const bottomOfPage = visible + scrollY >= pageHeight;
+      return bottomOfPage || pageHeight < visible;
+    }
+  }
 };
 </script>
 

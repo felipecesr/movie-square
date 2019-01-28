8b0cfb45742e6828891f895341966a5ecb7a5b66
Felipe César <felipecesr@gmail.com>
2019-01-28 10:34:05 -0300

    Restructuring router files

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HomePage from './components/pages/HomePage.vue';
+import routes from './routes';
 
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomePage
-    }
-  ]
+  routes
 });

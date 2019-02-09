8be0360dad0ad11f2fcb5427acfdf366be95c2c6
Felipe César <felipecesr@gmail.com>
2019-02-09 16:04:10 -0300

    Add PageDetails component

---
+++ src/components/pages/PageDetails.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-if="selectedMovie">
+    <router-link :to="{ name: 'Home' }">Voltar</router-link>
+    <img
+      :src="selectedMovie.backdrop_path | imageHost"
+      :alt="selectedMovie.title || selectedMovie.name"
+    >
+    <h1>{{ selectedMovie.title || selectedMovie.name }}</h1>
+    <p>{{ selectedMovie.overview }}</p>
+
+    <h2>Episódios</h2>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  props: {
+    id: {
+      required: true,
+      type: Number
+    }
+  },
+
+  computed: mapGetters(['selectedMovie']),
+
+  mounted() {
+    this.fetchMovie(this.id)
+  },
+
+  methods: mapActions(['fetchMovie'])
+}
+</script>
+
+<style>
+
+</style>

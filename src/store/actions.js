a79a19880d9433e8cc57c88857db36051d5eddf5
Felipe César <felipecesr@gmail.com>
2019-02-09 16:05:56 -0300

    Add new movie actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -14,3 +14,17 @@ export const fetchMovies = ({ dispatch, state }) => {
     .then(({ data }) => dispatch('receiveMoviesSuccess', data))
     .catch(error => dispatch('receiveMoviesError', error));
 };
+
+export const requestMovie = ({ commit }) => commit(types.REQUEST_MOVIE);
+export const receiveMovieSuccess = ({ commit }, data) => commit(types.RECEIVE_MOVIE_SUCCESS, data);
+export const receiveMovieError = ({ commit }, error) => commit(types.REQUEST_MOVIE_ERROR, error);
+
+export const fetchMovie = async ({ commit, dispatch, state }, id) => {
+  if (state.movies.length === 0) {
+    await dispatch('fetchMovies');
+  }
+
+  commit(types.SET_SELECTED_MOVIE, id);
+};
+
+export const setSelectedMovie = ({ commit }, id) => commit(types.SET_SELECTED_MOVIE, id);

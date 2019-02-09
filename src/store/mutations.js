7d7936514d04802c774db6a9aa0a8699c1e157e3
Felipe César <felipecesr@gmail.com>
2019-02-09 12:18:29 -0300

    Refactor store methods

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,14 +1,15 @@
 import * as types from './mutation-types';
 
 export default {
-  [types.REQUEST_MOVIES]() {},
-  [types.RECEIVE_MOVIES_SUCCESS](state, { data }) {
-    state.movies = state.movies.concat(data.results);
-    state.maxPage = data.total_pages;
-
-    if (state.page < state.maxPage) {
-      state.page++;
-    }
+  [types.REQUEST_MOVIES](state) {
+    state.loading = true;
+    state.page += 1;
   },
-  [types.REQUEST_MOVIES_ERROR]() {}
+  [types.RECEIVE_MOVIES_SUCCESS](state, { results }) {
+    state.movies = [...state.movies, ...results];
+    state.loading = false;
+  },
+  [types.REQUEST_MOVIES_ERROR](state) {
+    state.loading = false;
+  }
 };

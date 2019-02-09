6e1430147a23b7bb2d405f995412f2190a966d45
Felipe César <felipecesr@gmail.com>
2019-02-09 16:04:21 -0300

    Add new routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,20 @@
 export default [
   {
     path: '/',
-    name: 'home',
-    component: () => import('@/components/pages/HomePage.vue')
+    name: 'Home',
+    component: () => import('@/components/pages/PageHome.vue')
+  },
+  {
+    path: '/:id',
+    name: 'Details',
+    component: () => import('@/components/pages/PageDetails.vue'),
+    props: (route) => ({ id: Number.parseInt(route.params.id) })
+  },
+  {
+    path: '*',
+    name: 'NotFound',
+    redirect: {
+      name: '/'
+    }
   }
 ];

fef3f9833af1fac2edab5d9239f87de8f8a3f6af
Felipe César <felipecesr@gmail.com>
2019-02-09 12:11:48 -0300

    Fix ScrollLoader component

--- src/components/atoms/ScrollLoader.vue
+++ src/components/atoms/ScrollLoader.vue
@@ -8,12 +8,13 @@ import { mapActions } from 'vuex';
 export default {
   data() {
     return {
-      observer: null
+      observer: null,
+      prevX: 0
     };
   },
 
   mounted() {
-    this.observer = new IntersectionObserver(this.fetchMovies);
+    this.observer = new IntersectionObserver(this.handleObserver);
     this.observer.observe(this.$el);
   },
 
@@ -21,10 +22,24 @@ export default {
     this.observer.disconnect();
   },
 
-  methods: mapActions(['fetchMovies'])
+  methods: {
+    ...mapActions(['fetchMovies']),
+
+    handleObserver([ entry ]) {
+      const x = entry.boundingClientRect.x;
+
+      if (this.prevX > x) {
+        this.fetchMovies();
+      }
+
+      this.prevX = x;
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+div {
+  width: 10px;
+}
 </style>
--- src/components/atoms/__tests__/ScrollLoader.spec.js
+++ src/components/atoms/__tests__/ScrollLoader.spec.js
@@ -2,7 +2,7 @@ import { shallowMount } from '@vue/test-utils';
 import ScrollLoader from '@atoms/ScrollLoader.vue';
 
 describe('ScrollLoader.vue', () => {
-  let intersect;
+  let intersect, wrapper, fetchMovies;
 
   global.IntersectionObserver = function(cb) {
     intersect = cb;
@@ -12,20 +12,37 @@ describe('ScrollLoader.vue', () => {
     };
   };
 
+  const createIntersectObject = number => [{
+    isIntersecting: true,
+    boundingClientRect: { x: number }
+  }];
+
+  beforeEach(() => {
+    fetchMovies = jest.fn();
+
+    wrapper = shallowMount(ScrollLoader, {
+      methods: { fetchMovies },
+    });
+  });
+
   it('renders correctly', () => {
     expect(shallowMount(ScrollLoader).element).toMatchSnapshot();
   });
 
-  it('should call fetchMovies when ScrollLoader is intersecting', () => {
-    const fetchMovies = jest.fn();
+  it('should not call fetchMovies when component is intercepted and list is empty', () => {
+    intersect(createIntersectObject(20));
+    expect(fetchMovies).not.toHaveBeenCalled();
+  });
 
-    shallowMount(ScrollLoader, {
-      methods: { fetchMovies },
-    });
-
-    intersect([
-      { isIntersecting: true }
-    ]);
+  it('should call fetchMovies when component is intercepted and list is filled', () => {
+    wrapper.vm.prevX = 2875;
+    intersect(createIntersectObject(275));
     expect(fetchMovies).toHaveBeenCalled();
+  });
+
+  it('prevX should be equal boundingClientRect.x after the component is intercepted', () => {
+    const prevX = 320;
+    intersect(createIntersectObject(prevX));
+    expect(wrapper.vm.prevX).toEqual(prevX);
   });
 });

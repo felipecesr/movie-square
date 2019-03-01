0648ef64612c3dcb72a50cb2aeecd3561f4823b7
Felipe César <felipecesr@gmail.com>
2019-03-01 16:39:42 -0300

    Add MovieSeasons component

---
+++ src/components/organisms/MovieSeasons.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <select v-model="selectedSeason">
+      <option
+        v-for="season in seasons"
+        :key="season.id"
+        :value="season.season_number"
+      >{{ season.name }}</option>
+    </select>
+    <episode-list
+      :serie_id="serie"
+      :season_number="selectedSeason"
+    />
+  </div>
+</template>
+
+<script>
+import EpisodeList from "@organisms/EpisodeList.vue";
+
+export default {
+  components: {
+    EpisodeList
+  },
+
+  props: {
+    serie: {
+      required: true,
+      type: Number
+    },
+    seasons: {
+      required: true,
+      type: Array
+    }
+  },
+
+  data() {
+    return {
+      selectedSeason: 1
+    };
+  }
+};
+</script>
+
+<style>
+select {
+  border: 2px solid #fff;
+  padding: 0.5em;
+}
+</style>

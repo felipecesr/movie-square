29a1b5078d3d6a4853753a2da2c67d49ce1d2d96
Felipe César <felipecesr@gmail.com>
2019-03-01 19:46:06 -0300

    Add VTitle component

---
+++ src/components/atoms/VTitle.vue
@@ -0,0 +1,29 @@
+<template functional>
+  <h1>
+    <slot />
+  </h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  font-family: var(--font-secondary);
+  font-weight: normal;
+  color: var(--color-text-heading);
+  margin-bottom: var(--space-xxs);
+  line-height: var(--heading-line-height);
+
+  &::before {
+    content: "";
+    display: inline-block;
+    height: 2px;
+    width: 23px;
+    background-color: var(--color-accent);
+    vertical-align: middle;
+    margin-right: 20px;
+  }
+}
+</style>
